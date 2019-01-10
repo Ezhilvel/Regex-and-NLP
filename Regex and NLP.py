d44b356d277c9a52758b23ab42d440ed14914e82
Ezhilvel Malar Elangovan <ezhilvelme@gmail.com>
2019-01-09 17:16:27 -0800

    Add files via upload

--- Regex and NLP.py
+++ Regex and NLP.py
@@ -71,5 +71,66 @@ pattern2 = r"[\w\s]+:"
 print(re.search(pattern2, sentences[3]))
 
 
+# Import the necessary modules
+from nltk.tokenize import TweetTokenizer
+from nltk.tokenize import regexp_tokenize
+
+# Define a regex pattern to find hashtags: pattern1
+pattern1 = r"#\w+"
+
+# Use the pattern on the first tweet in the tweets list
+regexp_tokenize(tweets[0], pattern1)
+
+# Write a pattern that matches both mentions and hashtags
+pattern2 = r"([@|#]\w+)"
+
+# Use the pattern on the last tweet in the tweets list
+regexp_tokenize(tweets[-1], pattern2)
+
+# Use the TweetTokenizer to tokenize all tweets into one list
+tknzr = TweetTokenizer()
+all_tokens = [tknzr.tokenize(t) for t in tweets]
+print(all_tokens)
+
+#Non-ascii tokenization
+german_text = 'Wann gehen wir Pizza essen? 🍕 Und fährst du mit Über? 🚕  '
+
+# Tokenize and print all words in german_text
+all_words = word_tokenize(german_text)
+print(all_words)
+
+# Tokenize and print only capital words
+capital_words = r"[A-Z\Ü]\w+"
+print(regexp_tokenize(german_text, capital_words))
+
+# Tokenize and print only emoji
+emoji = "['\U0001F300-\U0001F5FF'|'\U0001F600-\U0001F64F'|'\U0001F680-\U0001F6FF'|'\u2600-\u26FF\u2700-\u27BF']"
+print(regexp_tokenize(german_text, emoji))
+
+
+from matplotlib import pyplot as plt
+
+# Split the script into lines: lines
+lines = scene_one.split('\n')
+
+# Replace all script lines for speaker
+pattern = "[A-Z]{2,}(\s)?(#\d)?([A-Z]{2,})?:"
+lines = [re.sub(pattern, '', l) for l in lines]
+
+# Tokenize each line: tokenized_lines
+tokenized_lines = [regexp_tokenize(s, r"\w+") for s in lines]
+
+# Make a frequency list of lengths: line_num_words
+line_num_words = [len(t_line) for t_line in tokenized_lines]
+
+# Plot a histogram of the line lengths
+plt.hist(line_num_words)
+
+# Show the plot
+plt.show()
+
+
+
+
 
 
